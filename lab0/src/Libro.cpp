7e3a1a6f24218d34849905c2d7a5520363587448
Nansaizu <fernanda.saizar@gmail.com>
2025-03-25 12:40:48 -0300

    me olvide de apretyar el commit si lol

---
+++ lab0/src/Libro.cpp
@@ -0,0 +1,30 @@
+#include "../include/Libro.h"
+
+Libro::Libro() : Publicacion(), m_Editorial(""), m_PalabrasDestacadas() {}
+
+Libro::Libro(const std::string &doi, const std::string &titulo, const DTFecha &fecha, const std::string &m_Editorial, const std::set<std::string> &m_PalabrasDestacadas)
+    : Publicacion(doi, titulo, fecha), m_Editorial(editorial), m_PalabrasDestacadas(palabrasDestacadas) {}
+
+Libro::~Libro() {}
+
+bool Libro::contienePalabra(const std::string &palabra) {
+
+    return m_PalabrasDestacadas.find(palabra) != m_PalabrasDestacadas.end();
+
+}
+
+std::string Libro::getEditorial() const {
+    return m_Editorial;
+}
+
+std::set<std::string> Libro::getPalabrasDestacadas() const {
+    return m_PalabrasDestacadas;
+}
+
+void Libro::setEditorial(const std::string &editorial) {
+    m_Editorial = editorial;
+}
+
+void Libro::setPalabrasDestacadas(const std::set<std::string> &palabras) {
+    m_PalabrasDestacadas = palabras;
+}
