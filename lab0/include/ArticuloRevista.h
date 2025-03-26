8273781184cc5effc8b2e098215341c11197c1ae
Mr-Monk3y <thiagorodriguezseg@gmail.com>
2025-03-26 19:49:13 -0300

    aaaaaaaaaaaaa

--- lab0/include/ArticuloRevista.h
+++ lab0/include/ArticuloRevista.h
@@ -7,18 +7,34 @@
 class ArticuloRevista : public Publicacion
 {
   public:
+  	// Constructores
 	ArticuloRevista();
+<<<<<<< Updated upstream
 	ArticuloRevista(const std::string &m_Revista,
 	                const std::string &m_Extracto);
+=======
+	ArticuloRevista(const std::string &doi, const std::string &titulo,
+	                const DTFecha &fecha, const std::string &revista,
+	                const std::string &extracto);
+
+	// Destructor				
+>>>>>>> Stashed changes
 	~ArticuloRevista();
 
-	std::string getRevista() const;
-	std::string getExtracto() const;
+	// Operación polimórfica
+	bool contienePalabra(const std::string &palabra);
 
+	// getters
 	void setRevista(const std::string &revista);
 	void setExtracto(const std::string &extracto);
 
+<<<<<<< Updated upstream
 	virtual bool contienePalabra(const std::string &palabra) override;
+=======
+	// setters
+	std::string getRevista() const;
+	std::string getExtracto() const;
+>>>>>>> Stashed changes
 
   private:
 	std::string m_Revista;
--- lab0/include/PaginaWeb.h
+++ lab0/include/PaginaWeb.h
@@ -7,14 +7,29 @@
 class PaginaWeb : public Publicacion
 {
   public:
+  	// Constructores
 	PaginaWeb();
+<<<<<<< Updated upstream
 	PaginaWeb(const std::string &m_URL, const std::string &m_ContenidoExtraido);
 	~PaginaWeb();
 	virtual bool contienePalabra(const std::string &palabra) override;
+=======
+	PaginaWeb(const std::string &doi, const std::string &titulo,
+	          const DTFecha &fecha, const std::string &url,
+	          const std::string &contenidoExtraido);
+>>>>>>> Stashed changes
 
+	// Destructor		  
+	~PaginaWeb();
+
+	// Operación polimórfica
+	bool contienePalabra(const std::string &palabra);
+
+	// getters
 	std::string getURL() const;
 	std::string getContenidoExtraido() const;
 
+	// setters
 	void setURL(const std::string &url);
 	void setContenidoExtraido(const std::string &contenido_extraido);
 
